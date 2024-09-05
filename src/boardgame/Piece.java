40e2e4e13ce4a732a89aaa788965782ed921b2e2
lucasGlla <lukinhapontes@outlook.com>
2024-09-04 23:49:36 -0300

    Starting to implement Board and Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,16 @@
+package boardgame;
+
+public class Piece {
+	protected Position position;
+	public Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
+	
+	protected Board getBoard() {
+		return board;
+	}
+
+}
